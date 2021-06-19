aae214c9d991b9b4cf0a9ba20b9e50cf39553e0a
Paul Groke <paul.groke@dynatrace.com>
2021-06-19 15:38:50 +0200

    work in progress

--- iscan.analyze/Program.cs
+++ iscan.analyze/Program.cs
@@ -7,24 +7,24 @@ using iscan.dm;
 
 namespace iscan.analyze
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var sw = Stopwatch.StartNew();
-            using (var file = File.OpenRead("_iscan_out.txt"))
-            {
-                var project = DMReader.ReadProject(file);
-                Console.WriteLine("Time: " + sw.ElapsedMilliseconds + " ms");
-                Console.WriteLine("TUs: " + project.tus.Length);
-                Console.WriteLine("Paths: " + project.paths.Length);
-                Console.WriteLine("Memory 0: " + GC.GetTotalMemory(false) / 1024 + " k");
-                for (int g = 0; g <= GC.MaxGeneration; g++)
-                    GC.Collect(g);
-                Console.WriteLine("Memory 1: " + GC.GetTotalMemory(true) / 1024 + " k");
-                GC.KeepAlive(project);
-            }
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var sw = Stopwatch.StartNew();
+			using (var file = File.OpenRead("_iscan_out.txt"))
+			{
+				var project = DMReader.ReadProject(file);
+				Console.WriteLine("Time: " + sw.ElapsedMilliseconds + " ms");
+				Console.WriteLine("TUs: " + project.tus.Length);
+				Console.WriteLine("Paths: " + project.paths.Length);
+				Console.WriteLine("Memory 0: " + GC.GetTotalMemory(false) / 1024 + " k");
+				for (int g = 0; g <= GC.MaxGeneration; g++)
+					GC.Collect(g);
+				Console.WriteLine("Memory 1: " + GC.GetTotalMemory(true) / 1024 + " k");
+				GC.KeepAlive(project);
+			}
+		}
+	}
 
 }
--- iscan.dm/DataModel.cs
+++ iscan.dm/DataModel.cs
@@ -4,23 +4,23 @@ using System.Text;
 
 namespace iscan.dm
 {
-    public class DMProject
-    {
-        public string[] paths { get; set; }
-        public DMTranslationUnit[] tus { get; set; }
-    }
+	public class DMProject
+	{
+		public string[] paths { get; set; }
+		public DMTranslationUnit[] tus { get; set; }
+	}
 
-    public class DMTranslationUnit
-    {
-        public int path { get; set; }
-        public DMFile[] files { get; set; }
-    }
+	public class DMTranslationUnit
+	{
+		public int path { get; set; }
+		public DMFile[] files { get; set; }
+	}
 
-    public class DMFile
-    {
-        public int path { get; set; }
-        public int stk { get; set; }
-        public int sln { get; set; }
-        public int[] inc { get; set; }
-    }
+	public class DMFile
+	{
+		public int path { get; set; }
+		public int stk { get; set; }
+		public int sln { get; set; }
+		public int[] inc { get; set; }
+	}
 }
